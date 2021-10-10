a3880c64a67e56c2e67d78a3ad97f90469c06448
Admunoz8 <60449029+Admunoz8@users.noreply.github.com>
2021-10-10 14:17:57 -0600

    Update riscv.c

--- riscv.c
+++ riscv.c
@@ -42,31 +42,28 @@ int findRegNum(char* str){
   return  atoi(num);
 }
 //this will be able to handle the strings as a int so we can use them  in our swithc stateement 
-int caseHandler(char* str){
-  if (areSame(str, "LW")) {return 0;}
-  else if (areSame(str, "SW")) {return 1;}
-  else if (areSame(str, "ADD")) {return 2;}
-  else if (areSame(str, "ADDI")) {return 3;}
-  else if (areSame(str, "AND")) {return 4;}
-  else if (areSame(str, "OR")) {return 5;}
-  else if (areSame(str, "XOR")) {return 6;}
+int caseH(char* str){
+  if (same(str, "LW")) {return 0;}
+  else if (same(str, "SW")) {return 1;}
+  else if (Same(str, "ADD")) {return 2;}
+  else if (same(str, "ADDI")) {return 3;}
   else
     return -1;
 }
 //this meethod helps my case handler help by letting me compare th given string
-bool areSame(char* str1, char* str2){
-  char* temp1 = str1;
-  char* temp2 = str2;
-  int count1 = 0;
+bool same(char* str1, char* str2){
+  char* tmp1 = str1;
+  char* tmp2 = str2;
+  int count = 0;
   int count2 = 0;
 
-  while (*(temp1+count1) != '\0'){
-    count1++;
+  while (*(tmp1+count) != '\0'){
+    count++;
   }
-  while (*(temp2+count2) != '\0'){
+  while (*(tmp2+count2) != '\0'){
     count2++;
   }
-  if (count1 != count2){return false;}
+  if (count != count2){return false;}
   
   int i = 0;
   while (*(str1+i) != '\0' && *(str2+i) != '\0'){
@@ -98,46 +95,46 @@ void init_regs(){
  */
  //this method is use so that we can properly interpert, it will use pointers 
 bool interpret(char* instr){
-  char* textFile = "mem.txt";
+  char* text = "mem.txt";
   char **pointerS;
   pointerS = tokenize(instr, ' ');
   //print_all_tokens(pointerStorage); use for debugging purposes.
-  int caseNum = caseHandler(pointerS[0]); // decides what case the instruction is.
-  switch(caseNum){
+  int caseN = caseH(pointerS[0]); // decides what case the instruction is.
+  switch(caseN){
     //this case is for LW,The lw instruction loads a word into a register from memory.
   case 0:
-    printf("LW read");
-    char* saveToLW = *(pointerS+1);
-    int saveToLWint = findRegNum(saveToLW);
-    char** memoryTokensLW = tokenize(*(pointerS+2), '('); // splits the main 3rd token in 2.
-    char** cleanMemoryLW = tokenize(*(memoryTokensLW+1),')'); // cleans the mem token to only reg.
+    printf("LW done");
+    char* saveLW = *(pointerS+1);
+    int saveLWint = findRegNum(saveLW);
+    char** memTokLW = tokenize(*(pointerS+2), '('); // splits the main 3rd token in 2.
+    char** cleanMemLW = tokenize(*(memTokLW+1),')'); // cleans the mem token to only reg.
 
-    char* memoryRegLW = *cleanMemoryLW;
-    int memoryRegLWint = findRegNum(memoryRegLW);
+    char* memRegLW = *cleanMemLW;
+    int memRegLWint = findRegNum(memRegLW);
 
-    int32_t addressLW = atoi(*memoryTokensLW) + memoryRegLWint;
-    int32_t readLW = read_address(addressLW, textFile);
-    reg[saveToLWint] = readLW; // register is given whatever was read in addressLW
+    int32_t addressLW = atoi(*memTokLW) + memRegLWint;
+    int32_t readLW = read_address(addressLW, text);
+    reg[saveLWint] = readLW; // register is given whatever was read in addressLW
     break;
     //this case is for SW,it will copy the data from a register to memory. The register is not changed. 
 
   case 1:
-    printf("SW read");
-    char* saveFromSW = *(pointerS+1);
-    int saveFromSWint = findRegNum(saveFromSW);
-    char** memoryTokensSW = tokenize(*(pointerS+2), '(');
-    char** cleanMemorySW = tokenize(*(memoryTokensSW+1), ')');
-    char* memoryRegSW = *cleanMemorySW;
-    int memoryRegSWint = findRegNum(memoryRegSW);
+    printf("SW done");
+    char* saveSW = *(pointerS+1);
+    int saveSWint = findRegNum(saveSW);
+    char** memTokSW = tokenize(*(pointerS+2), '(');
+    char** cleanMemSW = tokenize(*(memTokSW+1), ')');
+    char* memRegSW = *cleanMemSW;
+    int memRegSWint = findRegNum(memRegSW);
     
-    int32_t data_to_writeSW = reg[saveFromSWint];
-    int32_t addressSW = atoi(*memoryTokensSW) + memoryRegSWint; 
-    int32_t write = write_address(data_to_writeSW, addressSW, textFile);
+    int32_t datawriteSW = reg[saveSWint];
+    int32_t addressSW = atoi(*memTokSW) + memoryRegSWint; 
+    int32_t write = write_address(datawriteSW, addressSW, text);
     
     break;
     //Adds the first operand (destination operand) and the second operand (source operand) and stores the result in the destination operand.
   case 2:
-    printf("ADD read");
+    printf("ADD done");
     char* saveTo = *(pointerS+1);
     char* firstOperand = *(pointerS+2);
     char* secondOperand = *(pointerS+3);
@@ -151,7 +148,7 @@ bool interpret(char* instr){
     break;
     //This case is for ADDI instruction. Addi is used to  adds the immediate specified in the instruction to a value in a register
   case 3:
-    printf("ADDI read");
+    printf("ADDI done");
     char* saveToADDI = *(pointerS+1);
     char* addTo = *(pointerS+2);
     int add = atoi(*(pointerS+3));
@@ -177,7 +174,7 @@ int main(){
 	init_regs(); // DO NOT REMOVE THIS LINE
 
 	char input[1000]; // User input can only be 100 chars long.
-	printf("Enter 'x' to quit.\n");
+	printf("Enter 'ctrl+D to EOF' to quit.\n");
   //while loop so it can do the functions over and oveer until user press x, therefore it breaks
 	while(1){
 	  printf("Input: ");
